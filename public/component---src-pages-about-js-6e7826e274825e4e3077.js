3a61b5a87dd5249ede1ddb0f709604073d05b085
Mark Williams <mwill@Marks-MacBook-Pro.local>
2020-09-22 10:06:02 -0400

    Removed public folder

--- public/component---src-pages-about-js-6e7826e274825e4e3077.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"3XHS":function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),u=t("Bl7J");n.default=function(){return l.a.createElement(u.a,null,l.a.createElement("p",null,"About me placeholder..."))}}}]);
-//# sourceMappingURL=component---src-pages-about-js-6e7826e274825e4e3077.js.map
